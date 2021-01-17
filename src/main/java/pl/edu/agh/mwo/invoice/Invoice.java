7925e2209d3cddb4554f451fcb710bd5fa87d7bc
Dorota Puto <dootras@gmail.com>
2021-01-17 16:16:03 +0100

    InvoiceTest working

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,52 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
 
-    public void addProduct(Product product) {
-        // TODO: implement
-    }
+	private Map<Product, Integer> products = new LinkedHashMap<Product, Integer>();
 
-    public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
-    }
+	public void addProduct(Product product) {
+		addProduct(product, 1);
+	}
 
-    public BigDecimal getSubtotal() {
-        return null;
-    }
+	public void addProduct(Product product, Integer quantity) {
+		if (quantity <= 0) {
+			throw new IllegalArgumentException("Quantity must be more than 0");
+		}
+		products.put(product, quantity);
+	}
 
-    public BigDecimal getTax() {
-        return null;
-    }
+	public BigDecimal getNetPrice() {
+		
+		return this.products.keySet().stream()
+		.map(p -> p.getPrice().multiply(new BigDecimal(products.get(p))))
+		.reduce(BigDecimal.ZERO, BigDecimal::add);
+		
+//		BigDecimal sum = BigDecimal.ZERO;
+//		for (Product p : this.products.keySet()) {
+//			sum = sum.add(p.getPrice().multiply(new BigDecimal(products.get(p))));
+//		}
+//		return sum;
+	}
 
-    public BigDecimal getTotal() {
-        return null;
-    }
+	public BigDecimal getTax() {
+		return this.getGroosPrice().subtract(this.getNetPrice());
+	}
+
+	public BigDecimal getGroosPrice() {
+		return this.products.keySet().stream()
+				.map(p -> p.getPriceWithTax().multiply(new BigDecimal(products.get(p))))
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+		
+//		BigDecimal sum = BigDecimal.ZERO;
+//		for (Product p : this.products.keySet()) {
+//			sum = sum.add(p.getPriceWithTax().multiply(new BigDecimal(products.get(p))));
+//		}
+//		return sum;
+	}
 }
